dd3067ab1535310ff23606e0a4964aae5b819e58
pinilDissanayaka <pinildissanayaka@gmail.com>
2023-12-24 19:10:34 +0530

    Implement stack using list

---
+++ Python DSA/Stack.py
@@ -0,0 +1,46 @@
+class Stack(object):
+    def __init__(self, size) -> None:
+        self._top = -1
+        self._size = size
+        self._arr = list()
+        
+    def insert(self, data) -> None:
+        if(self._top >= self._size):
+            print("Stack overflow...")
+            return
+        else:
+            self._top += 1
+            self._arr[self._top] = data
+    
+    def pop(self, data):
+        if(self._top == -1):
+            print("Stack is empty..")
+            return
+        else:
+            popElement = self._arr[self._top]
+            self._top -= 1
+            return popElement
+        
+    def print(self):
+        if(self._top == -1):
+            print("Stack is empty..")
+            return
+        else:
+            for i in range(0, self._top):
+                print(self._arr[i], end=" ")
+    
+    def isEmpty(self):
+        if(self._top == -1):
+            print("Stack is empty..")
+            return
+        else:
+            print("Stack is not empty..")
+            return
+    
+    def size(self):
+        if(self._top == -1):
+            print("Stack is empty..")
+            return 0
+        else:
+            return (self._head - 1)
+        
